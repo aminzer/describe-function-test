37c7d3d50b955eebc68ba4827e45578754e8534f
aminzer <minzer.alexey@gmail.com>
2024-07-14 23:28:47 +0300

    fix tests

--- src/formatTestName/config.ts
+++ src/formatTestName/config.ts
@@ -1,3 +1,5 @@
 export const ignoredPathParts = ['src', '__tests__', '__test__'];
 
 export const testFileExtensions = ['.test.js', '.test.ts'];
+
+export const pathSeparatorRegex = /[/\\]/;
--- src/formatTestName/formatTestName.ts
+++ src/formatTestName/formatTestName.ts
@@ -1,12 +1,15 @@
-import { relative } from 'path';
+import { relative, sep } from 'path';
 import { toString as getAppRootPath } from 'app-root-path';
-import { ignoredPathParts, testFileExtensions } from './config';
+import { ignoredPathParts, pathSeparatorRegex, testFileExtensions } from './config';
 
 const formatTestName = (testFilePath: string): string => {
-  const relativeTestFilePath = relative(getAppRootPath(), testFilePath);
+  const relativeTestFilePath = relative(
+    getAppRootPath().replace(pathSeparatorRegex, sep),
+    testFilePath.replace(pathSeparatorRegex, sep),
+  );
 
   return relativeTestFilePath
-    .split(/[/\\]/)
+    .split(pathSeparatorRegex)
     .filter((token) => !!token && !ignoredPathParts.includes(token))
     .map((token) =>
       testFileExtensions.reduce(
